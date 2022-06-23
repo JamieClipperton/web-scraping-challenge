f1fb724b9768039d637fbaeab4aa6c773d6b5b38
JamieClipperton <babygatter@yahoo.com>
2022-06-22 21:44:28 -0500

    update

---
+++ mission_to_mars/.ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"]= "mongodb://localhost:27017/phone_app"
+mongo = PyMongo(app)
+
+@app.route("/")
+def index ():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update_one({}, {"$set":mars_data}, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
